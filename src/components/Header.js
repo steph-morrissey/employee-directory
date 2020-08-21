ae002afa8b7c1cf8c3d64ae9edca409f7e4c042d
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-08-21 21:54:32 +0100

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className='text-center py-3 bg-dark text-white'>
+      <h1>Employee Directory</h1>
+      <p>
+        Click on carrots to filter by heading or use the search box to narrow
+        results.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
